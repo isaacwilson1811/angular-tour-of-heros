080f9dea09a1ab669b72973004bdf1eaf98a76d6
isaacW <ISAAC.WILSON1811@STU.MTEC.EDU>
2021-07-22 09:51:38 -0600

    added services messages and async data fetch

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,19 +14,23 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
   constructor(
-    private heroService: HeroService
+    private heroService: HeroService,
+    private messageService: MessageService
   ) { }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
 
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+
 
 }
